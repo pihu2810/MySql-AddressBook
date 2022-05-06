1b35c51aee47783ce7002d1ca1f22b7246f9a7c8
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 16:37:26 +0530

    Create AddressBook Table

--- addressbookService.sql
+++ addressbookService.sql
@@ -1,1 +1,14 @@
 create database addressbookService;
+use addressbookService;
+CREATE TABLE addressBook (
+		id int not null auto_increment primary key,
+		firstname varchar(20) NOT NULL, 
+        lastname varchar(20) NOT NULL,
+        address varchar(100),
+        city varchar(20) NOT NULL,
+        state varchar(20) NOT NULL,
+        zip integer(6),
+        phonenumber varchar(10) NOT NULL,
+        email varchar(20) NOT NULL UNIQUE
+);
+desc addressBook;
