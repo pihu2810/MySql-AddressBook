cc8a344021e5db9f4dd55a2de5faeec5abda5b18
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 17:51:45 +0530

    Refactor Retrieve Queries

--- addressbookService.sql
+++ addressbookService.sql
@@ -89,3 +89,18 @@ select * from addressbooks;
 select * from addressbooks_name;
 select * from addressbooks_type;
 select * from addressbooks_abtype;
+
+SELECT * from addressbooks inner join addressbooks_name where city = 'Pune' OR state = 'MP';
+
+SELECT  city, state, COUNT(*) from addressbooks  where city = 'Pune' AND state = 'Maharashtra';
+
+SELECT * FROM addressbooks WHERE state = 'Maharashtra' ORDER BY firstname ASC;
+
+SELECT type, count(*) FROM addressbooks join addressbooks_type group by type;
+
+select * from addressbooks inner join addressbooks_type ON addressbooks.id = 1 and addressbooks_type.id IN(1,2);
+
+
+select * from addressbooks inner join addressbooks_name inner join addressbooks_type ON addressbooks.id = 1 and addressbooks_name.id= '1' and addressbooks_type.id IN (1);
+
+
