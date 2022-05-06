de2a0d3202c496381e64ffdba72469943a5cd93b
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 17:35:53 +0530

    Refactor count by type AddressBook table

--- addressbookService.sql
+++ addressbookService.sql
@@ -30,13 +30,4 @@ SELECT * from addressbook where city = 'Pune' OR state = 'MP';
 SELECT  city, state, COUNT(*) from addressbook where city = 'Pune' AND state = 'Maharashtra';
 SELECT * FROM addressbook WHERE state = 'Maharashtra' ORDER BY firstname ASC;
 SELECT * FROM addressbook WHERE state = 'Maharashtra' ORDER BY firstname DESC;
-ALTER  TABLE addressbook add ab_name varchar(20);
-alter table addressbook add ab_type char(20);
-
-update addressbook set ab_name = 'AddressBook1' where id = 2;
-update addressbook set ab_name = 'AddressBook2' where id = 1;
-update addressbook set ab_name = 'AddressBook3' where id IN (3,4);
-
-update addressbook set ab_type = 'family' where id = 1;
-update addressbook set ab_type = 'friends' where id = 2;
-update addressbook set ab_type = 'profession' where id IN(3,4);
+SELECT ab_type, COUNT(*) FROM addressbook GROUP BY ab_type;
