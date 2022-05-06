871a571b8f51b6e0b510972ca1553cbae28b4d8f
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 16:49:22 +0530

    Insert value AddressBook Table

--- addressbookService.sql
+++ addressbookService.sql
@@ -12,3 +12,10 @@ CREATE TABLE addressBook (
         email varchar(20) NOT NULL UNIQUE
 );
 desc addressBook;
+INSERT INTO addressbook VALUES
+(1,'Vishaka','kadam','Kothrud','Pune','Maharashtra','424112','1234567890', 'vishaka.m@gmail.com'),
+(2,'Kunal','Patil','Deccan','Pune','Maharashtra','420166','9191919291', 'kunal.p@gmail.com'),
+(3,'Shanvika','J','Andhra','Andhra Pradesh','AP','487879','4567890125', 'Shanvika.k@yahoo.com'),
+(4,'Kumar','k','Madhya Pradesh','Gwalior','MP','43342','6577567890', 'kumar.m@gmail.com');
+
+SELECT * FROM addressbook;
